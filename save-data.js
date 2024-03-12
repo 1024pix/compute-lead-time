d50ff40c3c9d24a80ca404e996053a59565f222c
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-03-12 18:52:42 +0100

    feat: handle idempotence for saveData
    knex does not handle onConflict with batchInsert, we recreate
    batchInsert with chunk and transaction so that we can use onConflict method.

--- save-data.js
+++ save-data.js
@@ -1,5 +1,12 @@
 import { knex } from 'db/knex-database-connection.js';
+import chunk from 'lodash/chunk';
 
 export async function saveData({ data }) {
-  return knex.batchInsert('transitions', data, 1000);
+  const chunks = chunk(data, 1000);
+
+  const trx = await knex.transaction();
+  for (const chunk of chunks)
+    await trx('transitions').insert(chunk).onConflict(['issueId', 'source', 'destination', 'timestamp']).ignore();
+
+  await trx.commit();
 }
--- tests/save-data.test.js
+++ tests/save-data.test.js
@@ -22,4 +22,26 @@ describe('save data', () => {
     const [{ count }] = await knex('transitions').count();
     expect(count).to.equal(1);
   });
+
+  it('should not persist duplicate transitions', async () => {
+    const transitions = [
+      {
+        issueId: 'issue #1',
+        timestamp: '2024-03-12T12:00:00',
+        source: 'Backlog',
+        destination: 'Ready for Dev',
+      },
+      {
+        issueId: 'issue #1',
+        timestamp: '2024-03-12T12:00:00',
+        source: 'Backlog',
+        destination: 'Ready for Dev',
+      },
+    ];
+
+    await saveData({ data: transitions });
+
+    const [{ count }] = await knex('transitions').count();
+    expect(count).to.equal(1);
+  });
 });
