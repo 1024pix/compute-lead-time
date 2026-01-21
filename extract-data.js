0489d3d3543e1bc862b942098613236c5f8ffd2a
Marie-Hélène Rots <mache@pix.fr>
2026-01-21 10:16:24 +0100

    fix and enhance

--- extract-data.js
+++ extract-data.js
@@ -1,34 +1,60 @@
-export async function extractData({ jira }) {
-  const JQL = `project = "PIX" AND assignee in (6380873100cb2fc3f987c9b1, 5f8d43f26bc6340068bc7aae, 6125042124912a0069e38346, 712020:3faa6a43-ec4d-40f9-9dff-01d762c7c67c, 625694763bf0f0007016df40, 712020:ba07efcf-be6f-4996-a77e-7234e352d3db) AND "Date of MEP[Date]" >= 2024-01-01 AND type NOT IN ("Sujet en conception", "Problème en investigation", Epic) AND parent not in (PIX-13545, PIX-13853)`;
-  const { total } = await jira.searchJira(JQL);
-  // eslint-disable-next-line no-console
-  console.log("Total issues", total);
-  const pages = Math.ceil(total / 100);
+const yvo = '5e37d584b341530e62accbb1';
+const gege = '712020:3faa6a43-ec4d-40f9-9dff-01d762c7c67c';
+const xavier = '5f8d43f26bc6340068bc7aae';
+const alex = '6380873100cb2fc3f987c9b1';
+const mache = '625694763bf0f0007016df40';
+const lionel = '712020:ba07efcf-be6f-4996-a77e-7234e352d3db';
+const vincent = '5cff529d1854c30c0132524d';
+const baptiste = '712020:025c7d85-14ab-48b5-ab09-6eb2b429a59d';
+const alice = '712020:ac7abe82-ce27-49b0-9db4-a88992869c17';
 
-  const issues = [];
-  for (let i = 0; i < pages; i++) {
-    // eslint-disable-next-line no-console
-    console.log("Extracting page", i + 1, "of", pages);
-    const pageIssues = await extractIssuesForGivenPage({
-      jira,
-      jql: JQL,
-      page: i,
-    });
-    issues.push(...pageIssues);
+export async function extractData({ jira }) {
+  const JQL = `project = PIX
+AND "Equipix[Select List (multiple choices)]" 
+  IN (Prescription, Cross-team, "Design System")
+AND assignee IN 
+  (${yvo}, 
+  ${gege}, 
+  ${xavier}, 
+  ${alex}, 
+  ${mache}, 
+  ${lionel}, 
+  ${vincent}, 
+  ${baptiste}, 
+  ${alice}
+  )
+AND issuetype 
+  NOT IN ( Epic,"Problème en investigation","Sujet en conception") 
+AND labels 
+  IN (null, EMPTY, Production, Produit, RGPD, Design, Tech, SECURITE, Accessibilité) 
+AND "Date of MEP[Date]" != null
+AND created >= 2025-01-01 
+AND parent not in (PIX-18537, PIX-18540)`;
+
+  let result = await jira.searchJira(JQL, { fields: ['*all'], expand: 'transitions, operations, changelog' });
+
+  const allIssues = [];
+
+  while (result.isLast === false) {
+    console.log('Extracting pages...');
+    allIssues.push(...result.issues);
+    result = await jira.searchJira(JQL, { nextPageToken: result.nextPageToken, fields: ['*all'], expand: 'transitions, operations, changelog' });
   }
-  return issues;
+
+  // for the last batch when is last is true
+  allIssues.push(...result.issues);
+
+  return await extractIssuesForGivenPage({
+    jira,
+    allIssues,
+  });
 }
 
-async function extractIssuesForGivenPage({ jira, jql, page }) {
-  const search = await jira.searchJira(jql, {
-    maxResults: 100,
-    startAt: page * 100,
-  });
-
+async function extractIssuesForGivenPage({ jira, allIssues }) {
   const issues = [];
-  for (const issue of search.issues) {
+  for (const issue of allIssues) {
     const issueStatusChangelog = await getStatusChangelog({
-      issueKey: issue.key,
+      issueId: issue.key,
       jira,
     });
     issues.push({
@@ -40,14 +66,14 @@ async function extractIssuesForGivenPage({ jira, jql, page }) {
   return issues;
 }
 
-async function getStatusChangelog({ issueKey, jira }) {
-  const issueChangelog = await jira.getIssueChangelog(issueKey);
+async function getStatusChangelog({ issueId, jira }) {
+  const issueChangelog = await jira.getIssueChangelog(issueId);
   return issueChangelog.values.flatMap(({ created, items }) => {
     return items
-      .filter(({ field }) => field === "status")
+      .filter(({ field }) => field === 'status')
       .map(({ fromString, toString }) => {
         return {
-          issueId: issueKey,
+          issueId,
           timestamp: created,
           source: fromString,
           destination: toString,
